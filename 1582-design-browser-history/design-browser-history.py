bd46ac58f21adfc602510372e616e89af172f786
Anjana Nallanagula <68939759+anjana502@users.noreply.github.com>
2026-04-02 08:08:50 -0600

    Time: 56 ms (20.71%) | Memory: 22.2 MB (29.04%) - LeetSync

---
+++ 1582-design-browser-history/design-browser-history.py
@@ -0,0 +1,48 @@
+class TreeNode:
+    def __init__(self, val = "", next = None, prev = None):
+        self.val = val
+        self.next = next
+        self.prev = prev
+    
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.front = TreeNode()
+        self.rear = TreeNode()
+        self.front.next = self.rear
+        self.rear.prev = self.front
+        self.curr = self.front
+
+        self.visit(homepage)
+
+    def visit(self, url: str) -> None:
+        new_node = TreeNode(url, self.rear, self.curr)
+        next1 = self.curr.next
+        new_node.next = next1
+        self.curr.next = new_node
+        new_node.prev = self.curr
+        next1.prev = new_node
+        self.curr = new_node
+
+        self.curr.next = self.rear
+        self.rear.prev = self.curr
+
+    def back(self, steps: int) -> str:
+        while (steps > 0 and self.curr.prev != self.front):
+            self.curr = self.curr.prev
+            steps -= 1
+        
+        return self.curr.val
+
+    def forward(self, steps: int) -> str:
+        while (steps > 0 and self.curr.next != self.rear):
+            self.curr = self.curr.next
+            steps -= 1
+        
+        return self.curr.val
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
